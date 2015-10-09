4e0608da22fc229881794e04fc48d6a4d399b239
unknown <meyerphilip96@googlemail.com>
2015-10-09 16:11:54 +0200

    added KeyMap

--- KeyMap.cpp
+++ KeyMap.cpp
@@ -11,6 +11,13 @@ void KeyMap::addKeyBind( unsigned short key, std::function<void()> Func)
 	KeyBindings[key].push_back(Func);
 }
 
+void KeyMap::onKeyPress(unsigned short key)
+{
+	std::vector<std::function<void()>> funcs = KeyBindings[key];
+
+	for (std::function<void()> f : funcs)
+		f();
+}
 
 KeyMap::~KeyMap()
 {
--- KeyMap.h
+++ KeyMap.h
@@ -2,6 +2,8 @@
 #include <vector>
 #include <functional>
 
+struct GLFWwindow;
+
 #pragma once
 class KeyMap
 {
@@ -10,11 +12,11 @@ public:
 
 	void addKeyBind( unsigned short key, std::function<void()> Func);
 
-	void onKeyPress();
+	void onKeyPress( unsigned short);
 
 	~KeyMap();
 
 	static KeyMap instance; //should be possibly modified for multiplay profiles
 private:
-	std::map<unsigned short, std::vector<std::function<void()>>> KeyBindings;
+	std::map<unsigned short, std::vector<std::function<void()>>> KeyBindings; // short inn binary 00000 3 bit modifier Key (Alt, Shift, Ctrl) 8 bit key
 };
