9531208328ed047c8a1bb18c11a2f356d7e47642
unknown <meyerphilip96@googlemail.com>
2015-10-10 21:52:59 +0200

    test

---
+++ Clock.cpp
@@ -0,0 +1,43 @@
+#include "Clock.h"
+
+
+
+Clock::Clock( std::function<void()> init, std::function<void()> f) : init(init), func(f), running(true)
+{}
+
+void Clock::run()
+{
+	running = true;
+	t = std::thread([this] {update(); });
+}
+
+Clock::~Clock()
+{
+	running = false;
+	if (t.joinable())
+		t.join();
+}
+
+void Clock::update()
+{
+	init();
+
+	while (running)
+	{
+		lastTick = std::chrono::system_clock::now();
+
+		
+		func();
+		currentTicks++;
+
+		std::this_thread::sleep_for(milliseconds((int)std::floorf(16.666f)) - duration_cast<std::chrono::milliseconds>(system_clock::now() - lastTick));
+
+
+		if (duration_cast<milliseconds>(system_clock::now() - lastSec) >= milliseconds(1000))
+		{
+			lastTicks = currentTicks;
+			currentTicks = 0;
+			lastSec = system_clock::now();
+		}
+	}
+}
---
+++ Clock.h
@@ -0,0 +1,36 @@
+#include <chrono>
+#include <functional>
+#include <thread>
+
+using namespace std::chrono;
+
+#pragma once
+class Clock
+{
+public:
+	Clock(std::function<void()>, std::function<void()>);
+
+	unsigned int getLastTPS() { return lastTicks; }
+
+	void run();
+
+	bool isRunning() { return running; }
+	void shutdown() { running = false; }
+
+	~Clock();
+private:
+	//function Variable
+	std::function<void()> init, func;
+	//Loop Variables
+	bool running;
+
+	unsigned short currentTicks = 0; //tps
+	unsigned short lastTicks = 0;
+	system_clock::time_point lastSec = std::chrono::system_clock::now();  //used to update TPS
+	system_clock::time_point lastTick = std::chrono::system_clock::now(); //used to retain ticking Speed
+
+	//Loop
+	void update();
+
+	std::thread t;
+};
